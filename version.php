c4c33823829e040530e2d5aca60c843ab7f8f91d
georgmaisser <georg.maisser@wunderbyte.at>
2022-12-20 22:13:29 +0100

    Version 0.1.1 (2022122000)

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_catquiz';
-$plugin->release = '0.1.0';
-$plugin->version = 2022112900;
+$plugin->release = '0.1.1';
+$plugin->version = 2022122000;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_ALPHA;
