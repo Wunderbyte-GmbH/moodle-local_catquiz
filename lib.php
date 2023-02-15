6635778213a28b76a4c20b14b12ada63f0e03dbf
Georg Maißer <georg.maisser@wunderbyte.at>
2023-02-15 08:37:45 +0100

    Take out unneeded menu entry

--- lib.php
+++ lib.php
@@ -48,9 +48,6 @@ function local_catquiz_render_navbar_output(\renderer_base $renderer) {
             <a class="dropdown-item" href="'
                 . $CFG->wwwroot . '/local/catquiz/manage_catscales.php"">'
                 . get_string('managecatscales', 'local_catquiz') . '</a>
-            <a class="dropdown-item" href="'
-                . $CFG->wwwroot . '/local/catquiz/test.php">'
-                . get_string('test', 'local_catquiz') . '</a>
         </div>
     </div>';
 
