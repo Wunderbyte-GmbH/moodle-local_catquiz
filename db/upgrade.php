cd7bf2692f5608151da0da074db2d9fb7d625325
Georg Maißer <georg.maisser@wunderbyte.at>
2023-03-09 01:01:22 +0100

    Add new test table

--- db/upgrade.php
+++ db/upgrade.php
@@ -59,10 +59,42 @@ function xmldb_local_catquiz_upgrade($oldversion) {
             if (!$dbman->field_exists($table, $field)) {
                 $dbman->add_field($table, $field);
             }
-    
+
             // Catquiz savepoint reached.
             upgrade_plugin_savepoint(true, 2023012504, 'local', 'catquiz');
         }
+
+        if ($oldversion < 2023030700) {
+
+            // Define table local_catquiz_tests to be created.
+        $table = new xmldb_table('local_catquiz_tests');
+
+        // Adding fields to table local_catquiz_tests.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('componentid', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
+        $table->add_field('component', XMLDB_TYPE_CHAR, '255', null, null, null, '');
+        $table->add_field('name', XMLDB_TYPE_CHAR, '255', null, null, null, '0');
+        $table->add_field('description', XMLDB_TYPE_TEXT, null, null, null, null, null);
+        $table->add_field('descriptionformat', XMLDB_TYPE_INTEGER, '2', null, null, null, '1');
+        $table->add_field('json', XMLDB_TYPE_TEXT, null, null, null, null, null);
+        $table->add_field('visible', XMLDB_TYPE_INTEGER, '2', null, null, null, '1');
+        $table->add_field('availability', XMLDB_TYPE_CHAR, '255', null, null, null, '');
+        $table->add_field('lang', XMLDB_TYPE_CHAR, '30', null, null, null, '');
+        $table->add_field('timecreated', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
+        $table->add_field('timemodified', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
+
+        // Adding keys to table local_catquiz_tests.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+
+        // Conditionally launch create table for local_catquiz_tests.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+            // Catquiz savepoint reached.
+            upgrade_plugin_savepoint(true, 2023030700, 'local', 'catquiz');
+        }
+
 
     return true;
 }
