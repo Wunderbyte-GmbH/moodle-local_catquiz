37d986cfeb7ead7d0c4e2755b0c07292a83e3fd3
Daniel Pasterk <daniel@pasterk.net>
2023-06-12 10:36:56 +0200

    add dev validation file

---
+++ dev/validation_callable_composition.php
@@ -0,0 +1,78 @@
+<?php
+use local_catquiz\catcontext;
+use local_catquiz\local\model\model_item_param;
+use local_catquiz\local\model\model_item_param_list;
+use local_catquiz\local\model\model_person_param;
+use local_catquiz\local\model\model_person_param_list;
+use local_catquiz\local\model\model_responses;
+use local_catquiz\local\model\model_strategy;
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ *  Code for validation of developing process;
+ *
+ * @package local_catquiz
+ * @author Daniel Pasterk
+ * @copyright 2023 Wunderbyte GmbH
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once('../../../config.php');
+//use \local_catquiz;
+
+$PAGE->set_url(new moodle_url('/local/catquiz/workspace.php'));
+$PAGE->set_context(context_system::instance());
+$PAGE->set_pagelayout('frontpage');
+$url_front = new moodle_url('/workspace.php');
+$url_plugin = new moodle_url('workspace.php');
+
+echo $OUTPUT->header();
+
+print "hehe";
+
+
+
+$sum = 0;
+$mycallable = function($x) {return 0;};
+
+$my_x = 3;
+
+$history = [];
+
+$callables = [];
+for ($i=1; $i<=100;$i++){
+
+    $rnd = rand(0,10);
+    array_push($history,$rnd);
+
+    $funpart = function ($x) use ($rnd) {return $rnd*$x;};
+    //$funpart = function ($x) use ($rnd) {return rand(0,100)*$x;};
+
+
+    $sum = $sum + $funpart($my_x);
+
+    $mycallable = \local_catquiz\mathcat::compose_plus($mycallable, $funpart);
+    array_push($callables,$mycallable);
+
+
+}
+
+
+
+echo "finished";
+
+echo $OUTPUT->footer();
