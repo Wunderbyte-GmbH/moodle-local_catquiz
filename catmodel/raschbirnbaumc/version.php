8fe963d7f3f7bff0d7cdc7e9f2521247aa1cdac4
David Szkiba <david.szkiba@wunderbyte.at>
2023-06-15 14:59:36 +0200

    GH-36 Add subplugin DB field `guessing`

--- catmodel/raschbirnbaumc/version.php
+++ catmodel/raschbirnbaumc/version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022122000;
+$plugin->version   = 2023061500;
 $plugin->requires  = 2022041900;
 $plugin->component = 'catmodel_raschbirnbaumc';
 
