5a607e8d60c4f4465b3bc454ea2850540f926e97
David Szkiba <david.szkiba@wunderbyte.at>
2023-06-16 16:18:53 +0200

    GH-37 Calculate fisher information for questions

--- classes/teststrategy/teststrategy_fastest.php
+++ classes/teststrategy/teststrategy_fastest.php
@@ -16,6 +16,7 @@
 
 namespace local_catquiz\teststrategy;
 
+use local_catquiz\local\model\model_strategy;
 use moodle_exception;
 
 /**
@@ -29,8 +30,11 @@ class teststrategy_fastest extends teststrategy {
      */
     public int $id = STRATEGY_FASTEST;
 
-    public function __construct() {
+    // Classnames of installed CAT models indexed by model name
+    private array $installed_models = [];
 
+    public function __construct() {
+        $this->installed_models = model_strategy::get_installed_models();
     }
 
         /**
@@ -51,12 +55,49 @@ class teststrategy_fastest extends teststrategy {
             throw new moodle_exception('nowquestionsincatscale', 'local_catquiz');
         }
 
-        //
-
+        // TODO: Not hardcoded context
+        $contextid = 1;
+        $person_ability = $this->get_user_ability($contextid);
+        foreach ($questions as $question) {
+            if (!array_key_exists($question->model, $this->installed_models)) {
+                throw new moodle_exception('missingmodel', 'local_catquiz');
+            }
+            $model = $this->installed_models[$question->model];
+            $question->fisher_information = $model::fisher_info($person_ability, [floatval($question->difficulty)]);
+        }
+        usort($questions, function($q1, $q2) {
+            return $q2->fisher_information <=> $q1->fisher_information;
+        });
+        // now $questions[0] is the one with the maximum fisher information
 
         $index = rand(0, count($questions)-1);
 
         return $questions[$index];
+    }
+
+    /**
+     * TODO: Use caching for better performance
+     * 
+     * @param int $contextid
+     * @return float 
+     */
+    private function get_user_ability(int $contextid): float {
+        global $DB, $USER;
+
+        $person_param = $DB->get_record(
+            'local_catquiz_personparams',
+            [
+                'userid' => $USER->id,
+                'contextid' => $contextid,
+            ]
+        );
+
+        // Use default ability of 0 if we have no ability for that user
+        if (empty($person_param)) {
+            return 0.0;
+        }
+
+        return $person_param->ability;
     }
 
     /**
