ecc5b27ed6b74a5ab2c3a8930f87bb5bb09321b6
David Szkiba <david.szkiba@wunderbyte.at>
2023-06-23 16:34:28 +0200

    GH-42 Add teststrategy_balanced

---
+++ classes/teststrategy/strategy/teststrategy_balanced.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace local_catquiz\teststrategy\strategy;
+
+use local_catquiz\teststrategy\strategy;
+
+/**
+ * Will select questions in a way that balances the general number of attempts
+ */
+class teststrategy_balanced extends strategy {
+
+    /**
+     *
+     * @var int $id // strategy id defined in lib.
+     */
+    public int $id = STRATEGY_BALANCED;
+
+    public function requires_score_modifiers(): array {
+        return [
+            'numberofgeneralattempts',
+            'strategybalancedscore',
+        ];
+    }
+}
