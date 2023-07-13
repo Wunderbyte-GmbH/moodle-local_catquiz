774ee438bbdcd60eff7c25f60e76427c1f53dade
David Szkiba <david.szkiba@wunderbyte.at>
2023-07-13 16:06:18 +0200

    Codestyle: remove copied comment

--- classes/teststrategy/item_score_modifier/maximumquestionscheck.php
+++ classes/teststrategy/item_score_modifier/maximumquestionscheck.php
@@ -10,7 +10,6 @@ use local_catquiz\wb_middleware;
 final class maximumquestionscheck extends item_score_modifier implements wb_middleware
 {
     public function run(array $context, callable $next): result {
-        // If there are no pilot questions available, then return a random normal question
         if ($context['questionsattempted'] >= $context['maximumquestions']) {
             return result::err(status::ERROR_REACHED_MAXIMUM_QUESTIONS);
         }
