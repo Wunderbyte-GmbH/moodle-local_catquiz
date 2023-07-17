c75b9cedd9b2c12a196ae59fdf94c6fb13719db0
David Szkiba <david.szkiba@wunderbyte.at>
2023-07-17 10:37:06 +0200

    GH-43 Select models for items to update ability
    Changes the hardcoded value that selected `raschbirnbauma` as the model for each
    item to a function call that returns a list of items with the appropriate model
    set for each item.

--- classes/teststrategy/item_score_modifier/updatepersonability.php
+++ classes/teststrategy/item_score_modifier/updatepersonability.php
@@ -7,6 +7,7 @@ use local_catquiz\catcalc;
 use local_catquiz\catcontext;
 use local_catquiz\catquiz;
 use local_catquiz\local\model\model_item_param_list;
+use local_catquiz\local\model\model_strategy;
 use local_catquiz\local\result;
 use local_catquiz\local\status;
 use local_catquiz\teststrategy\item_score_modifier;
@@ -24,13 +25,21 @@ final class updatepersonability extends item_score_modifier implements wb_middle
 
     public function run(array $context, callable $next): result {
         global $USER;
-        $item_param_list = model_item_param_list::load_from_db($context['contextid'], 'raschbirnbauma'); // TODO not hardcoded
-        $responses = catcontext::create_response_from_db($context['contextid'])
-            ->as_array(); // Get the list of responses by this user
-        $components = $responses[$USER->id];
+        $responses = catcontext::create_response_from_db($context['contextid']);
+
+        // Select the correct model for each item
+        $model_strategy = new model_strategy($responses);
+        $item_param_lists = [];
+        foreach (array_keys($model_strategy->get_installed_models()) as $model) {
+            $item_param_lists[$model] = model_item_param_list::load_from_db($context['contextid'], $model);
+        }
+        $item_param_list = $model_strategy->select_item_model($item_param_lists);
+
+        $components = ($responses->as_array())[$USER->id];
         if (count($components) > 1) {
             throw new moodle_exception('User has answers to more than one component.');
         }
+
         $userresponses = reset($components);
 
         // If the last answer was incorrect and the question was excluded due to
