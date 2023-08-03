a5d57e28c669f796f196b1ce26badc14b71b7f5d
Magdalena <georgmaisser@Davids-Mac-mini.local>
2023-08-03 12:16:03 +0200

    version 0.1.4 (2023080300)

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_catquiz';
-$plugin->release = '0.1.3';
-$plugin->version = 2023072400;
+$plugin->release = '0.1.4';
+$plugin->version = 2023080300;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->dependencies = [
