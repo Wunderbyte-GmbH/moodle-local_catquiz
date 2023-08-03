96e65a09c30e0294715ad75dcd231c449f76bd34
David Szkiba <david.szkiba@wunderbyte.at>
2023-08-03 17:29:11 +0200

    GH-48 Add interface methods for TR jacobian and hessian

--- classes/catcalc_item_estimator.php
+++ classes/catcalc_item_estimator.php
@@ -29,5 +29,7 @@ interface catcalc_item_estimator
 {
     public static function get_log_jacobian($ability, float $item_response);
     public static function get_log_hessian($ability, float $item_response);
+    public static function get_log_tr_jacobian(array $parameters): array;
+    public static function get_log_tr_hessian(array $parameters): array;
     public static function get_model_dim(): int;
 }
