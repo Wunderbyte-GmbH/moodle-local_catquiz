fc0bfeb0985a9f3b34372a248397b09817f4d1e5
David Szkiba <david.szkiba@wunderbyte.at>
2023-09-05 14:42:40 +0200

    WIP

---
+++ tests/mathcat_test.php
@@ -0,0 +1,51 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Tests the mathcat functionality.
+ *
+ * @package    catquiz
+ * @author David Szkiba <david.szkiba@wunderbyte.at>
+ * @copyright  2023 Georg Maißer <info@wunderbyte.at>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_catquiz;
+
+use basic_testcase;
+
+/**
+ * @package local_catquiz
+ * @covers \local_catquiz\mathcat
+ */
+class mathcat_test extends basic_testcase
+{
+
+    /**
+     * Test if the newton raphson multi stable function returns expected outputs.
+     *
+     * @return void
+     */
+    public function test_newton_raphson_multi_stable() {
+        $result = mathcat::newton_raphson_multi_stable(
+            fn ($x) => [1],
+            fn ($x) => [1],
+            ['difficulty' => 0]
+        );
+        $expected = ['difficulty' => 1];
+        $this->assertEquals($expected, $result);
+    }
+}
