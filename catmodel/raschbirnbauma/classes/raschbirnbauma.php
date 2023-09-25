f5a19d2e66898e34f4514169c25423e9677557f1
ralferlebach <ralf.erlebach@gmx.de>
2023-09-25 20:37:08 +0200

    Update raschbirnbauma.php
    Implemented:
    * LORS approach
    
    Updated:
    * $pp as array['ability'] at all methods
    * pre-calculate expensive EXP-functions (5 to 6 times faster now)
    
    ToDo:
    * interface definition has to be updated
    * calls for all mehods shold be now xxx(array('ability' => $pp, ...
    * some pre-calculations in the Trusted Regions methods
    * lines 62-93 seems to be deprecated/unsued and should be removed

--- catmodel/raschbirnbauma/classes/raschbirnbauma.php
+++ catmodel/raschbirnbauma/classes/raschbirnbauma.php
@@ -96,152 +96,258 @@ class raschbirnbauma extends model_raschmodel {
     /**
      * Calculates the Likelihood for a given the person ability parameter
      *
-     * @param float $pp - person ability parameter
-     * @param array $ip - item parameters ('difficulty')
+     * @param array <float> $pp - person ability parameter ('ability')
+     * @param array <float> $ip - item parameters ('difficulty')
      * @param float $k - answer category (0 or 1.0)
      * @return float
-     *
      */
-    public static function likelihood($pp, array $ip, float $k): float {
+    public static function likelihood(array $pp, array $ip, float $k): float {
+        $pp = $pp['ability'];
         $a = $ip['difficulty'];
+        
         if ($k < 1.0) {
             return 1 / (1 + exp($pp - $a));
         } else {
             return 1 / (1 + exp($a - $pp));
         }
     }
+    
     // Calculate the LOG Likelihood and its derivatives.
 
     /**
      * Calculates the LOG Likelihood for a given the person ability parameter
      *
-     * @param float $pp - person ability parameter
+     * @param array <float> $pp - person ability parameter ('ability')
      * @param array $ip - item parameters ('difficulty')
      * @param float $k - answer category (0 or 1.0)
-     * @return float
+     * @return float - log likelihood
      */
-    public static function log_likelihood($pp, array $ip, float $k): float {
+    public static function log_likelihood(array $pp, array $ip, float $k): float {
         return log(self::likelihood($pp, $ip, $k));
     }
 
+    /**
+     * Calculates the 1st derivative of the LOG Likelihood with respect to the item parameters
+     *
+     * @param float $pp - person ability parameter
+     * @param float $k - answer category (0 or 1.0)
+     * @return float - 1st derivative of log likelihood with respect to $pp
+     */
     public static function log_likelihood_p(array $pp, array $ip, float $k): float {
-        $pp = $pp[0];
+        $pp = $pp['ability'];
         $a = $ip['difficulty'];
+        
+        // Pre-Calculate high frequently used exp-terms.
+        $exp_a = exp($a);
+        $exp_p = exp($pp);
+        
         if ($k < 1.0) {
-            return -exp($pp) / (exp($a) + exp($pp));
+            return -$exp_p / ($exp_a + $exp_p);
         } else {
-            return exp($a) / (exp($a) + exp($pp));
+            return $exp_a / ($exp_a + $exp_p);
         }
     }
 
+    /**
+     * Calculates the 2nd derivative of the LOG Likelihood with respect to the person ability parameter
+     *
+     * @param array<float> $pp - person ability parameter
+     * @param array<float> $ip - item parameters ('difficulty')
+     * @param float $k - answer category (0 or 1.0)
+     * @return float - 2nd derivative of log likelihood with respect to $pp
+     */
     public static function log_likelihood_p_p(array $pp, array $ip, float $k): float {
+        $pp = $pp['ability'];
         $a = $ip['difficulty'];
-        return - (exp($a + $pp[0]) / ((exp($a) + exp($pp[0])) ** 2));
+        
+        // Pre-Calculate high frequently used exp-terms.
+        $exp_a = exp($a);
+        $exp_p = exp($pp);
+        
+        return - ($exp_a * $exp_p) / (($exp_a + $exp_p) ** 2);
     }
 
-    public static function get_log_jacobian($pp, array $ip, float $k): array {
+    /**
+     * Calculates the 1st derivative of the LOG Likelihood with respect to the item parameters
+     *
+     * @param array<float> $pp - person ability parameter ('ability')
+     * @param array<float> $ip - item parameters ('difficulty')
+     * @param float $k - answer category (0 or 1.0)
+     * @return array - jacobian vector
+     */
+    public static function get_log_jacobian(array $pp, array $ip, float $k): array {
+        $pp = $pp['ability'];
+        $a = $ip['difficulty'];
+        
+        $jacobian = [];
+        
+        // Pre-Calculate high frequently used exp-terms.
+        $exp_a = exp($a);
+        $exp_p = exp($pp);
+        
         if ($k >= 1.0) {
-            return [
-                (-exp($ip['difficulty'] + $pp) / ((exp($ip['difficulty']) + exp($pp)) * (exp($pp)))) // The d/da .
-            ];
+            $jacobian[0] = -($exp_a * $exp_p) / (($exp_a + $exp_p) * $exp_p) // The d/da .
         } else {
-            return [
-                (exp($pp) / (exp($ip['difficulty']) + exp($pp))) // The d/da .
-            ];
+            $jacobian[0] = $exp_p / ($exp_a + $exp_p) // The d/da .
         }
+        return $jacobian;
     }
 
-    public static function get_log_hessian($pp, $ip, float $k): array {
+    /**
+     * Calculates the 2nd derivative of the LOG Likelihood with respect to the item parameters
+     *
+     * @param array<float> $pp - person ability parameter ('ability')
+     * @param array<float> $ip - item parameters ('difficulty')
+     * @param float $k - answer category (0 or 1.0)
+     * @return array - hessian matrx
+     */
+    public static function get_log_hessian(array $pp, $ip, float $k): array {
+        $pp = $pp['ability'];
+        $a = $ip['difficulty'];
+        
+        $hessian = [[]];
+
+        // Pre-Calculate high frequently used exp-terms.        
+        $exp_a = exp($a);
+        $exp_p = exp($pp);
+
         // 2nd derivative is equal for both k = 0 and k = 1
-        return [[
-            -exp($ip['difficulty'] + $pp) / (exp($ip['difficulty']) + exp($pp)) ** 2 // The d²/ da² .
-        ]];
+        $hessian[0][0] = -($exp_a * $exp_p) / ($exp_a + $exp_p) ** 2 // The d²/ da² .
+        return $hessian; 
     }
 
     // Calculate the Least-Mean-Squres (LMS) approach.
+    
     /**
      * Calculates the Least Mean Squres (residuals) for a given the person ability parameter and a given expected/observed score
      *
-     * @param array $pp - person ability parameter
-     * @param array $ip - item parameters ('difficulty', 'discrimination', 'guessing')
-     * @param array $k - fraction of correct (0 ... 1.0)
-     * @param array $n - number of observations
+     * @param array<float> $pp - person ability parameter ('ability')
+     * @param array<float> $ip - item parameters ('difficulty', 'discrimination', 'guessing')
+     * @param float $k - fraction of correct (0 ... 1.0)
+     * @param float $n - number of observations
      * @return float - weighted residuals
      */
-    public static function least_mean_squares(array $pp, array $ip, array $k, array $n): float {
-        $lmsresiduals = 0;
-        $numbertotal = 0;
-
-        foreach ($pp as $key => $ability) {
-            if (!(is_float($n[$key]) && is_float($k[$key]))) {
-                continue;
-            }
-
-            $lmsresiduals += $n[$key] * ($k[$key] - self::likelihood($ability, $ip, 1.0)) ** 2;
-            $numbertotal += $n[$key];
-        }
-        return (($numbertotal > 0) ? ($lmsresiduals / $numbertotal) : (0));
+   function least_mean_squares(array $pp, array $ip, float $k, float $n):float{
+        return $n * ($k - likelihood($pp, $ip, 1.0)) ** 2;
     }
 
     /**
      * Calculates the 1st derivative of Least Mean Squres with respect to the item parameters
      *
-     * @param array $pp - person ability parameter
-     * @param array $ip - item parameters ('difficulty', 'discrimination')
-     * @param array $k - fraction of correct (0 ... 1.0)
-     * @param array $n - number of observations
-     * @return array - 1st derivative
+     * @param array<float> $pp - person ability parameter ('ability')
+     * @param array<float> $ip - item parameters ('difficulty')
+     * @param float $k - fraction of correct (0 ... 1.0)
+     * @param float $n - number of observations
+     * @return array - 1st derivative of lms with respect to $ip
      */
-    public static function least_mean_squares_1st_derivative_ip(array $pp, array $ip, array $k, array $n): array {
-        $derivative = 0;
+   function least_mean_squares_1st_derivative_ip(array $pp, array $ip, float $k, float $n):array{
+        $pp = $pp['ability'];
         $a = $ip['difficulty'];
 
-        foreach ($pp as $key => $ability) {
-            if (!(is_numeric($n[$key]) && is_numeric($k[$key]))) {
-                continue;
-            }
+        $derivative = [];
+        
+        // Pre-Calculate high frequently used exp-terms.
+        $exp_ap0 = exp($a - $pp);
+        
+        $derivative[0] = $n * (2 * $exp_ap0 * ($k - 1 + $exp_ap0 * $k)) / (1 + $exp_ap0) ** 3; // Calculate d/da.            
 
-            $derivative += $n[$key] * (2 * exp($a + $ability) * (exp($a)
-                * $k[$key] + exp($ability) * ($k[$key]) - 1)) / (exp($a) + exp($ability)) ** 3; // Calculate d/da.
-        }
-        return [$derivative];
+        return $derivative;
     }
 
     /**
      * Calculates the 2nd derivative of Least Mean Squres with respect to the item parameters
      *
-     * @param array $pp - person ability parameter
-     * @param array $ip - item parameters ('difficulty', 'discrimination')
-     * @param array $k - fraction of correct (0 ... 1.0)
-     * @param array $n - number of observations
-     * @return array - 1st derivative
-     */
-    public static function least_mean_squares_2nd_derivative_ip(array $pp, array $ip, array $k, array $n): array {
-        $derivative = [[0]];
+     * @param array<float> $pp - person ability parameter ('ability')
+     * @param array<float> $ip - item parameters ('difficulty')
+     * @param float $k - fraction of correct (0 ... 1.0)
+     * @param float $n - number of observations
+     * @return array - 2nd derivative of lms with respect to $ip
+     */ 
+   function least_mean_squares_2nd_derivative_ip(array $pp, array $ip, float $k, float $n):array{
+        $pp = $pp['ability'];
         $a = $ip['difficulty'];
 
-        foreach ($pp as $key => $ability) {
-            if (!(is_numeric($n[$key]) && is_numeric($k[$key]))) {
-                continue;
-            }
-
-            // Calculate d²/da².
-            $derivative[0][0]  += $n[$key] * (2 * exp($a + $ability) *
-                                (2 * exp($a + $ability) + exp(2 * $ability) * (-1 + $k[$key]) - exp(2 * $a) * $k[$key]))
-                                / (exp($a) + exp($ability)) ** 4;
-        }
+        $derivative = [[]];
+        
+        // Pre-Calculate high frequently used exp-terms.
+        $exp_bap1 = exp($a + $pp);
+        $exp_bap0 = exp($a - $pp);
+        $exp_a = exp($a);
+        $exp_p = exp($pp):
+        
+        $derivative[0][0]  = $n * (2 * $b ** 2 * ($exp_a * $exp_p) * (-$exp_p ** 2 + 2 * ($exp_a * $exp_p) + (-$exp_a ** 2 + $exp_p ** 2) * $k)) / ($exp_a + $exp_p) ** 4; // Calculate d²2/da². 
+              
         return $derivative;
     }
 
+    // Calculate the Log'ed Odds-Ratio Squared (LORS) approach.
+    
+    /**
+     * Calculates the Log'ed Odds-Ratio Squared (residuals) for a given the person ability parameter and a given expected/observed score
+     *
+     * @param array<float> $pp - person ability parameter ('ability')
+     * @param array<float> $ip - item parameters ('difficulty')
+     * @param float $or - odds ratio
+     * @param float $n - number of observations
+     * @return float - weighted residuals
+     */   
+    function lors_residuals(array $pp, array $ip, float $or, float $n = 1):float {
+        $pp = $pp['ability'];
+        $a = $ip['difficulty'];
+        
+        return $n * (log($or) + ($a - $pp)) ** 2;
+    }
+
+    /**
+     * Calculates the 1st derivative of Log'ed Odds-Ratio Squared with respect to the item parameters
+     *
+     * @param array<float> $pp - person ability parameter ('ability')
+     * @param array<float> $ip - item parameters ('difficulty')
+     * @param float $or - odds ratio
+     * @param float $n - number of observations
+     * @return array - 1st derivative
+     */   
+   function lors_1st_derivative_ip(array $pp, array $ip, float $or, float $n = 1): array {
+        $pp = $pp['ability'];
+        $a = $ip['difficulty'];
+       
+        $derivative = [];
+
+        $derivative[0] = $n * 2 * ($a - $pp + log($or)); // Calculate d/da.            
+        
+        return $derivative;
+    }
+    
+    /**
+     * Calculates the 2nd derivative of Log'ed Odds-Ratio Squared with respect to the item parameters
+     *
+     * @param array<float> $pp - person ability parameter ('ability')
+     * @param array<float> $ip - item parameters ('difficulty')
+     * @param float $or - odds ratio
+     * @param float $n - number of observations
+     * @return array - 1st derivative
+     */   
+   function lors_2nd_derivative_ip(array $pp, array $ip, float $or, float $n = 1): array {
+        $pp = $pp['ability'];
+        $a = $ip['difficulty'];
+        
+        $derivative = [[]];
+        
+        $derivative[0][0]  = $n * 2; // Calculate d²2/da².            
+      
+        return $derivative;
+    }
+    
     // Calculate Fisher-Information.
 
     /**
-     * Calculates the Fisher Information for a given person ability parameter.
+     * Calculates the Fisher Information for a given person ability parameter
      *
-     * @param float $pp
-     * @param array $ip
+     * @param array<float> $pp - person ability parameter ('ability')
+     * @param array<float> $ip - item parameters ('difficulty')
      * @return float
      */
+
     public static function fisher_info(float $pp, array $ip) {
         return (self::likelihood($pp, $ip, 0) * self::likelihood($pp, $ip, 1.0));
     }
@@ -251,8 +357,8 @@ class raschbirnbauma extends model_raschmodel {
     /**
      * Implements a Filter Function for trusted regions in the item parameter estimation
      *
-     * @param array $ip
-     * return array
+     * @param array<float> $ip - item parameters ('difficulty')
+     * return array - chunked item parameter
      */
     public static function restrict_to_trusted_region(array $ip): array {
         // Set values for difficulty parameter.
@@ -282,9 +388,10 @@ class raschbirnbauma extends model_raschmodel {
     /**
      * Calculates the 1st derivative trusted regions for item parameters
      *
-     * @return array
+     * @param array<float> $ip - item parameters ('difficulty')
+     * @return array - 1st derivative of TR function with respect to $ip
      */
-    public static function get_log_tr_jacobian(): array {
+    public static function get_log_tr_jacobian(array $ip): array {
         // Set values for difficulty parameter.
         $am = 0; // Mean of difficulty.
         $as = 2; // Standard derivation of difficulty.
@@ -292,22 +399,23 @@ class raschbirnbauma extends model_raschmodel {
         $atr = floatval(get_config('catmodel_raschbirnbauma', 'trusted_region_factor_sd_a'));
 
         return [
-            fn ($ip) => (($am - $ip['difficulty']) / ($as ** 2)) // The d/da .
+            ($am - $ip['difficulty']) / ($as ** 2) // The d/da .
         ];
     }
 
     /**
      * Calculates the 2nd derivative trusted regions for item parameters
      *
-     * @return array
+     * @param array<float> $ip - item parameters ('difficulty')
+     * @return array<array> - 2nd derivative of TR function with respect to $ip
      */
-    public static function get_log_tr_hessian(): array {
+    public static function get_log_tr_hessian(array $ip): array {
         // Set values for difficulty parameter.
         $am = 0; // Mean of difficulty.
         $as = 2; // Standard derivation of difficulty.
 
         return [[
-            fn ($x) => (-1 / ($as ** 2)) // Calculate d/da d/da.
+            -1 / ($as ** 2) // Calculate d/da d/da.
         ]];
     }
 }
