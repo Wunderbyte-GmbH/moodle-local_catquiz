3671a5bb7314e5dfe209a14f7e569f7c697f13a0
David Szkiba <david.szkiba@toolani.com>
2023-09-26 10:17:09 +0200

    Bump version number
    I added a new capability local/catquiz:view_teacher_feedback and increasing the version number seems to be required to make it available.

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_catquiz';
 $plugin->release = '0.1.4';
-$plugin->version = 2023091904;
+$plugin->version = 2023092600;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->dependencies = [
