7ca56a6e5c601c263178a81247008fa8b223284f
David Szkiba <david.szkiba@toolani.com>
2023-10-06 08:41:00 +0200

    Tests: check that the default CAT context is present

---
+++ tests/catcontext_test.php
@@ -0,0 +1,47 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Tests the catcontext functionality.
+ *
+ * @package    catquiz
+ * @author David Szkiba <david.szkiba@wunderbyte.at>
+ * @copyright  2023 Georg Maißer <info@wunderbyte.at>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_catquiz;
+
+use basic_testcase;
+
+/**
+ * @package local_catquiz
+ * @covers \local_catquiz\catcontext
+ */
+class catcontext_test extends basic_testcase
+{
+
+    /**
+     * Test if there is a default CAT context.
+     *
+     * @return void
+     */
+    public function test_default_context_is_installed() {
+        global $DB;
+        $contexts = $DB->get_records('local_catquiz_catcontext');
+        $this->assertGreaterThan(0, count($contexts));
+    }
+}
