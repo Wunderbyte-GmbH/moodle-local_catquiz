dea556a46b8cf692fc51677cebe748b5446a6837
David Szkiba <david.szkiba@wunderbyte.at>
2024-01-09 10:45:29 +0100

    Calculate fisher information for every scale
    Instead of calculating it only for the root scale, change the fisherinformation
    property on the question class from a scalar to an array and store the FI for
    each scale.

--- classes/teststrategy/feedbackgenerator/debuginfo.php
+++ classes/teststrategy/feedbackgenerator/debuginfo.php
@@ -275,7 +275,7 @@ class debuginfo extends feedbackgenerator {
             $questionsperscale = [];
             if (array_key_exists('questions', $data)) {
                 foreach ($data['questions'] as $qid => $question) {
-                    $fisherinformation = $question->fisherinformation ?? "NA";
+                    $fisherinformation = $question->fisherinformation[$data['catscaleid']] ?? "NA";
                     $score = $question->score ?? "NA";
                     $questions[] = [
                         'id' => $qid,
