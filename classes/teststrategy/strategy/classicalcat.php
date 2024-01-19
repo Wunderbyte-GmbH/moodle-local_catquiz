3c2d3af5e37a03648e05701137f259fb540dfef1
David Szkiba <david.szkiba@wunderbyte.at>
2024-01-19 16:55:00 +0100

    Cleanup: remove unused code
    This removes code that calculated the standarderror for each scale based
    on the ability in the root scale.

--- classes/teststrategy/strategy/classicalcat.php
+++ classes/teststrategy/strategy/classicalcat.php
@@ -82,8 +82,8 @@ class classicalcat extends strategy {
             mayberemovescale::class,
             firstquestionselector::class,
             fisherinformation::class,
-            addscalestandarderror::class,
             updatepersonability::class,
+            addscalestandarderror::class,
             strategyclassicscore::class,
         ];
     }
