3618bb3114356ab421941f57e923584791a87eff
David Szkiba <david.szkiba@wunderbyte.at>
2024-01-29 09:48:06 +0100

    WIP

---
+++ tests/mini_test.php
@@ -0,0 +1,9 @@
+<?php
+namespace local_catquiz; // Your module name.
+use basic_testcase; // Moodle test class.
+
+class mini_test extends basic_testcase {
+    public function test_example() {
+        $this->assertTrue(false); // This will fail.
+    }
+}
