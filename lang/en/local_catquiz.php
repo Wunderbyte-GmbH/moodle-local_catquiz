43c490faec228f1da4a758442b66342bfad39b6e
Magdalena Holczik <magdalena.holczik@wunderbyte.at>
2024-01-31 12:48:26 +0100

    GH-273 matching of imported scalenames via parentscalenames

--- lang/en/local_catquiz.php
+++ lang/en/local_catquiz.php
@@ -542,7 +542,7 @@ $string['scaleinformation'] = 'The id of the CAT scale the item should be assign
 $string['scalenameinformation'] = 'The name of the CAT scale the item should be assigned to. If no catscale id given, matching is done via name.';
 $string['dataincomplete'] = 'Record with componentid {$a->id} is incomplete and could not be treated entirely. Check field "{$a->field}".';
 $string['modelinformation'] = 'This field is necessary to entirely treat the record. If it is empty, item can only be assigned to CAT scale.';
-$string['parentscalenamesinformation'] = 'You can enter parent scales for the defined scale, in order to create the correct scale strucutre on the fly. Start with the highest parent and separate all children with "|" (vertical line unicode U+007C - do not mistake for slash "/").';
+$string['parentscalenamesinformation'] = 'To match the an item via the scalename, make sure to name all parent scales including root scale. For new - yet to be created - scales, you can enter parent scales for the defined scale. Start with the highest parent and separate all children with "|" (vertical line unicode U+007C - do not mistake for slash "/").';
 
 // Testenvironments table.
 $string['notifyallteachers'] = 'Notify all teachers';
