b62a5c40f645a3d3facaa042fee1b693fbd490b2
Andrii Semenets <semteacher@gmail.com>
2024-01-31 10:46:31 +0200

    Validation for min questions per scale <= max questions per test. (no German string)

--- lang/en/local_catquiz.php
+++ lang/en/local_catquiz.php
@@ -112,7 +112,7 @@ $string['formelementnegative'] = 'Input a positive number';
 $string['formelementwrongpercent'] = 'Input a number from 0 to 100';
 $string['formminquestgreaterthan'] = 'Minimum must be less than maximum';
 $string['formelementbetweenzeroandone'] = 'Please enter values between 0 and 1.';
-
+$string['formmscalegreaterthantest'] = 'Per scale minimum must be less than per test maximum';
 
 // Test Strategy.
 $string['catquiz_teststrategyheader'] = 'CAT Settings';
