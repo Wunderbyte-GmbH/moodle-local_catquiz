61e1ce6b3bd7f96be6eb4a4687b5da3b7de25882
Magdalena Holczik <magdalena.holczik@wunderbyte.at>
2024-03-10 10:33:28 +0100

    GH-294 move teacherfeedback into tabs with debuginfo last

--- classes/teststrategy/strategy/inferlowestskillgap.php
+++ classes/teststrategy/strategy/inferlowestskillgap.php
@@ -119,10 +119,10 @@ class inferlowestskillgap extends strategy {
         return [
             new customscalefeedback($this->feedbacksettings),
             new comparetotestaverage($this->feedbacksettings),
-            new debuginfo($this->feedbacksettings),
             new personabilities($this->feedbacksettings),
             new questionssummary($this->feedbacksettings),
             new graphicalsummary($this->feedbacksettings),
+            new debuginfo($this->feedbacksettings),
         ];
     }
 
