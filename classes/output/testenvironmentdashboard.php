bd5f2658ed939b8af27b320d73d3d0f88a346f3b
Magdalena Holczik <magdalena.holczik@wunderbyte.at>
2024-03-14 15:57:12 +0100

    fix: remove useless actionbuttons in tests & templates table

--- classes/output/testenvironmentdashboard.php
+++ classes/output/testenvironmentdashboard.php
@@ -155,35 +155,6 @@ class testenvironmentdashboard implements renderable, templatable {
         $table->showreloadbutton = true;
         $table->addcheckboxes = true;
 
-        $table->actionbuttons[] = [
-            'label' => get_string('notifyteachersofselectedcourses', 'local_catquiz'), // Name of your action button.
-            // The method needs to be added to your child of wunderbyte_table class.
-            'methodname' => 'notifyteachersofselectedcourses',
-            'class' => 'btn btn-primary',
-            'href' => '#',
-            'id' => -1, // This forces single call execution.
-            'nomodal' => false,
-            'selectionmandatory' => true,
-            // Will be added eg as data-id = $values->id, so values can be transmitted to the method above.
-            'data' => [
-                'id' => 'id',
-                'name' => 'name',
-            ],
-            ];
-
-        $table->actionbuttons[] = [
-            'label' => get_string('notifyallteachers', 'local_catquiz'), // Name of your action button.
-            'methodname' => 'notifyallteachers', // The method needs to be added to your child of wunderbyte_table class.
-            'class' => 'btn btn-primary',
-            'href' => '#',
-            'id' => -1, // This forces single call execution.
-            'nomodal' => false,
-            'data' => [ // Will be added eg as data-id = $values->id, so values can be transmitted to the method above.
-                'id' => 'id',
-                'name' => 'name',
-            ],
-            ];
-
         list($idstring, $encodedtable, $html) = $table->lazyouthtml(10, true);
         return $html;
     }
