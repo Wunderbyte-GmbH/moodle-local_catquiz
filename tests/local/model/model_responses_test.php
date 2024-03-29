a3e90b269ba7e5a82ebdf4fcbc6b85b7c9f98e0c
David Szkiba <david.szkiba@wunderbyte.at>
2024-03-29 16:34:15 +0100

    GH-393 Better tests for filtering responses
    It is now possible to limit both the users and items in a model_responses object. This will be needed to create smaller datasets for testing. The added tests ensure it works as expected.

--- tests/local/model/model_responses_test.php
+++ tests/local/model/model_responses_test.php
@@ -26,6 +26,8 @@
 namespace local_catquiz\local\model;
 
 use basic_testcase;
+use SebastianBergmann\RecursionContext\InvalidArgumentException;
+use PHPUnit\Framework\ExpectationFailedException;
 
 /**
  * Contains tests for the model_responses class.
@@ -35,17 +37,99 @@ use basic_testcase;
  */
 class model_responses_test extends basic_testcase {
 
-    public function test_filtering_values_works_as_expected() {
+    /**
+     * Tests if reducing the model_responses to items or persons works as expected
+     *
+     * @dataProvider filtering_values_works_as_expected_provider
+     *
+     * @return void
+     * @throws InvalidArgumentException
+     * @throws ExpectationFailedException
+     */
+    public function test_filtering_values_works_as_expected(
+        array $indata,
+        ?array $users,
+        ?array $items,
+        array $expectedusers,
+        array $expecteditems
+    ) {
         $mr = new model_responses();
-        $mr->set('P1', 'A1', 1.0);
-        $mr->set('P2', 'A2', 1.0);
-        $responses = $mr
-            ->limit_to_users(['P1'])
-            ->get_item_response();
-        $this->assertEquals(
-            $responses,
-            ['A1' => ['P1' => new model_item_response('A1', 1, (new model_person_param('P1'))->set_ability(0.34))]]
-        );
+        foreach ($indata as $id) {
+            $mr->set($id[0], $id[1], $id[2]);
+        }
+        if ($users) {
+            $mr->limit_to_users($users);
+        }
+        if ($items) {
+            $mr->limit_to_items($items);
+        }
+        $this->assertEquals($expectedusers, $mr->get_person_ids());
+        $this->assertEquals($expecteditems, $mr->get_item_ids());
+    }
+
+    /**
+     * Data provider for test_filtering_values_works_as_expected
+     *
+     * @return array
+     */
+    public static function filtering_values_works_as_expected_provider(): array {
+        return [
+            'limit persons' => [
+                'in_data' => [
+                    ['P1', 'A1', 1.0],
+                    ['P1', 'A2', 1.0],
+                    ['P1', 'A3', 1.0],
+                    ['P2', 'A1', 1.0],
+                    ['P2', 'A2', 1.0],
+                    ['P2', 'A3', 1.0],
+                    ['P3', 'A1', 1.0],
+                    ['P3', 'A2', 1.0],
+                    ['P3', 'A3', 1.0],
+                ],
+                'limit_to_users' => [
+                    'P1',
+                ],
+                'limit_to_items' => null,
+                'expected_users' => ['P1'],
+                'expected_items' => ['A1', 'A2', 'A3'],
+            ],
+            'limit items' => [
+                'in_data' => [
+                    ['P1', 'A1', 1.0],
+                    ['P1', 'A2', 1.0],
+                    ['P1', 'A3', 1.0],
+                    ['P2', 'A1', 1.0],
+                    ['P2', 'A2', 1.0],
+                    ['P2', 'A3', 1.0],
+                    ['P3', 'A1', 1.0],
+                    ['P3', 'A2', 1.0],
+                    ['P3', 'A3', 1.0],
+                ],
+                'limit_to_users' => null,
+                'limit_to_items' => [
+                    'A1',
+                ],
+                'expected_users' => ['P1', 'P2', 'P3'],
+                'expected_items' => ['A1'],
+            ],
+            'limit users and items' => [
+                'in_data' => [
+                    ['P1', 'A1', 1.0],
+                    ['P1', 'A2', 1.0],
+                    ['P1', 'A3', 1.0],
+                    ['P2', 'A1', 1.0],
+                    ['P2', 'A2', 1.0],
+                    ['P2', 'A3', 1.0],
+                    ['P3', 'A1', 1.0],
+                    ['P3', 'A2', 1.0],
+                    ['P3', 'A3', 1.0],
+                ],
+                'limit_to_users' => ['P1', 'P3'],
+                'limit_to_items' => [ 'A1', 'A2'],
+                'expected_users' => ['P1', 'P3'],
+                'expected_items' => ['A1', 'A2'],
+            ],
+        ];
     }
 
     public function test_setting_values_works_as_expected() {
