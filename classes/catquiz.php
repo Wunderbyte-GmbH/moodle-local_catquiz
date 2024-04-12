1d4c49f14a956f6bbb30d42718a16e947bc5c9b3
David Szkiba <david.szkiba@wunderbyte.at>
2024-04-12 14:43:04 +0200

    GH-12 Fix behat test: show correct test name
    Show the name of the test instead of the quiz instance

--- classes/catquiz.php
+++ classes/catquiz.php
@@ -731,7 +731,7 @@ class catquiz {
         $from = "
         ( SELECT
             ct.id,
-            aq.name,
+            name,
             component,
             componentid,
             c.visible,
