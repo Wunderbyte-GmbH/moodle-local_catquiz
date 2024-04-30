d2ff80893f31e10975158874cd34a0b2db0b64c7
David Szkiba <david.szkiba@wunderbyte.at>
2024-04-30 14:12:53 +0200

    Bump version

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_catquiz';
-$plugin->release = '0.9.3-rc7';
-$plugin->version = 2024042900;
+$plugin->release = '0.9.4-rc8';
+$plugin->version = 2024043000;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->dependencies = [
