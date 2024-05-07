b904d9274da101de010b73be8dbc157385d2ffe8
David Szkiba <david.szkiba@wunderbyte.at>
2024-05-07 12:24:32 +0200

    GH-479 Fix and add unit tests
    Now the tests do not only check if only courses of primary scales are returned if the "only-primary" setting is set to true. It also checks that if that setting is set to false, then courses of non-primary scales will be returned as well.

--- tests/output/attemptfeedback_test.php
+++ tests/output/attemptfeedback_test.php
@@ -25,7 +25,7 @@
 
 namespace local_catquiz;
 
-use basic_testcase;
+use advanced_testcase;
 use local_catquiz\output\attemptfeedback;
 use PHPUnit\Framework\ExpectationFailedException;
 use SebastianBergmann\RecursionContext\InvalidArgumentException;
@@ -40,20 +40,28 @@ use SebastianBergmann\RecursionContext\InvalidArgumentException;
  *
  * @covers \local_catquiz\output\attemptfeedback
  */
-class attemptfeedback_test extends basic_testcase {
+class attemptfeedback_test extends advanced_testcase {
     /**
      * Checks if the attemptfeedback class returns the correct course enrolment data.
      *
      * @param array $expected
      * @param array $feedbackdata
      * @param array $quizsettings
+     * @param bool $onlyprimary
      *
      * @return void
      * @throws InvalidArgumentException
      * @throws ExpectationFailedException
      * @dataProvider strategy_returns_expected_courses_to_enrol_provider
      */
-    public function test_it_returns_expected_courses_to_enrol(array $expected, array $feedbackdata, array $quizsettings) {
+    public function test_it_returns_expected_courses_to_enrol(
+        array $expected,
+        array $feedbackdata,
+        array $quizsettings,
+        bool $onlyprimary = true
+    ) {
+        $this->resetAfterTest(true);
+        set_config('enrol_only_to_primary_scale', $onlyprimary, 'local_catquiz');
         // We use a mock object so that we can work with the quiz settings and feedbackdata that we get from the data provider.
         // The rest of the attemptfeedback class is unchanged.
         $attemptfeedback = $this->getMockBuilder(attemptfeedback::class)
@@ -87,16 +95,8 @@ class attemptfeedback_test extends basic_testcase {
             sprintf('enrolment_message_checkbox_%s_1', $scaleid) => true,
             sprintf('enrolment_message_checkbox_%s_2', $scaleid) => false,
         ];
-        $feedbackdata = [
-            'personabilities_abilities' => [
-                1 => [
-                    'value' => 1.2,
-                    'name' => 'Simulation',
-                ],
-            ],
-        ];
         return [
-            'Should enrol to two courses' => [
+            'Only primary: should enrol to two courses' => [
                 'expected' => [
                     $scaleid => [
                         'range' => 2,
@@ -104,11 +104,30 @@ class attemptfeedback_test extends basic_testcase {
                         'course_ids' => [2, 4],
                     ],
                 ],
-                'feedbackdata' => $feedbackdata,
-                'quizsettings' => $quizsettings,
+                'feedbackdata' => [
+                    'personabilities_abilities' => [
+                        1 => [
+                            'value' => 1.2,
+                            'name' => 'Simulation',
+                            'primary' => true,
+                        ],
+                        2 => [
+                            'value' => 1.2,
+                            'name' => 'Another scale with courses',
+                        ]
+                    ],
+                ],
+                'quizsettings' => array_merge(
+                    $quizsettings,
+                    [
+                        'feedback_scaleid_limit_lower_2_1' => 0,
+                        'feedback_scaleid_limit_upper_2_1' => 5,
+                        'catquiz_courses_2_1' => [3],
+                    ]
+                ),
             ],
             // The ability is outside the defined ranges.
-            'Should enrol to no course' => [
+            'Only primary: should enrol to no course' => [
                 'expected' => [
                     $scaleid => [
                         'course_ids' => [],
@@ -119,10 +138,47 @@ class attemptfeedback_test extends basic_testcase {
                         1 => [
                             'value' => 6.0,
                             'name' => 'Simulation',
+                            'primary' => true,
                         ],
                     ],
                 ],
                 'quizsettings' => $quizsettings,
+            ],
+            'All scales: should enrol to multiple courses from different scales' => [
+                'expected' => [
+                    $scaleid => [
+                        'range' => 2,
+                        'show_message' => false,
+                        'course_ids' => [2, 4],
+                    ],
+                    2 => [
+                        'range' => 1,
+                        'show_message' => false,
+                        'course_ids' => [3, 4],
+                    ]
+                ],
+                'feedbackdata' => [
+                    'personabilities_abilities' => [
+                        1 => [
+                            'value' => 1.2,
+                            'name' => 'Simulation',
+                            'primary' => true,
+                        ],
+                        2 => [
+                            'value' => 1.2,
+                            'name' => 'Another scale with courses',
+                        ]
+                    ],
+                ],
+                'quizsettings' => array_merge(
+                    $quizsettings,
+                    [
+                        'feedback_scaleid_limit_lower_2_1' => 0,
+                        'feedback_scaleid_limit_upper_2_1' => 5,
+                        'catquiz_courses_2_1' => [3, 4],
+                    ]
+                ),
+                'onlyprimary' => false,
             ],
         ];
     }
@@ -133,6 +189,7 @@ class attemptfeedback_test extends basic_testcase {
      * @param array $expected
      * @param array $feedbackdata
      * @param array $quizsettings
+     * @param bool $onlyprimary
      *
      * @return void
      *
@@ -140,7 +197,14 @@ class attemptfeedback_test extends basic_testcase {
      * @throws ExpectationFailedException
      * @dataProvider strategy_returns_expected_groups_to_enrol_provider
      */
-    public function test_it_returns_expected_groups_to_enrol(array $expected, array $feedbackdata, array $quizsettings) {
+    public function test_it_returns_expected_groups_to_enrol(
+        array $expected,
+        array $feedbackdata,
+        array $quizsettings,
+        bool $onlyprimary = true
+    ) {
+        $this->resetAfterTest(true);
+        set_config('enrol_only_to_primary_scale', $onlyprimary, 'local_catquiz');
         // We use a mock object so that we can work with the quiz settings and feedbackdata that we get from the data provider.
         // The rest of the attemptfeedback class is unchanged.
         $attemptfeedback = $this->getMockBuilder(attemptfeedback::class)
@@ -172,24 +236,24 @@ class attemptfeedback_test extends basic_testcase {
             sprintf('catquiz_group_%s_1', $scaleid) => "1,3",
             sprintf('catquiz_group_%s_2', $scaleid) => "2,4",
         ];
-        $feedbackdata = [
-            'personabilities_abilities' => [
-                1 => [
-                    'value' => 1.2,
-                    'name' => 'Simulation',
-                ],
-            ],
-        ];
         return [
-            'Should enrol to two groups' => [
+            'Only primary: should enrol to two groups' => [
                 'expected' => [
                     1 => [2, 4],
                 ],
-                'feedbackdata' => $feedbackdata,
+                'feedbackdata' => [
+                    'personabilities_abilities' => [
+                        1 => [
+                            'value' => 1.2,
+                            'name' => 'Simulation',
+                            'primary' => true,
+                        ],
+                    ],
+                ],
                 'quizsettings' => $quizsettings,
             ],
             // The ability is outside the defined ranges.
-            'Should enrol to no group' => [
+            'Only primary: should enrol to no group' => [
                 'expected' => [
                     1 => [],
                 ],
@@ -198,10 +262,39 @@ class attemptfeedback_test extends basic_testcase {
                         1 => [
                             'value' => 6.0,
                             'name' => 'Simulation',
+                            'primary' => true,
                         ],
                     ],
                 ],
                 'quizsettings' => $quizsettings,
+            ],
+            'All scales: should enrol to multiple groups' => [
+                'expected' => [
+                    1 => [2, 4],
+                    2 => [3, 4, 5],
+                ],
+                'feedbackdata' => [
+                    'personabilities_abilities' => [
+                        1 => [
+                            'value' => 1.2,
+                            'name' => 'Simulation',
+                            'primary' => true,
+                        ],
+                        2 => [
+                            'value' => 1.3,
+                            'name' => 'Another scale',
+                        ],
+                    ],
+                ],
+                'quizsettings' => array_merge(
+                    $quizsettings,
+                    [
+                        'feedback_scaleid_limit_lower_2_1' => 0,
+                        'feedback_scaleid_limit_upper_2_1' => 5,
+                        'catquiz_group_2_1' => "3,4,5",
+                    ]
+                ),
+                'onlyprimary' => false,
             ],
         ];
     }
