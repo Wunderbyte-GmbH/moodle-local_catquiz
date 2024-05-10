fa2cb797c6805068b53aa8db6ab9b2b7a6e76a33
David Szkiba <david.szkiba@wunderbyte.at>
2024-05-10 15:22:26 +0200

    Fix codestyle and test

--- classes/local/model/model_strategy.php
+++ classes/local/model/model_strategy.php
@@ -403,14 +403,13 @@ class model_strategy {
     private function create_installed_models(): array {
         /** @var array<model_model> $instances */
         $instances = [];
-        $ignorelist = ['mixedraschbirnbaum'];
+        $ignorelist = ['mixedraschbirnbaum', 'grmgeneralized'];
 
         foreach (self::get_installed_models() as $name => $classname) {
-            $modelclass = new $classname();
             if (in_array($name, $ignorelist)) {
                 continue;
             }
-            $instances[$name] = $modelclass;
+            $instances[$name] = model_model::get_instance($name);
         }
         return $instances;
     }
