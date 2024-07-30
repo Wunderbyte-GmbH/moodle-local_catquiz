745f945c97b706ea3bb510cf10dce9dbee4d9282
ralferlebach <ralf.erlebach@gmx.de>
2024-07-30 09:55:53 +0200

    Fix Codecheck

--- classes/catquiz.php
+++ classes/catquiz.php
@@ -719,6 +719,8 @@ class catquiz {
      * @param array $testitemids
      * @param array $contextids
      * @param array $studentids
+     * @param int $starttime
+     * @param int $endtime
      *
      * @return array
      *
