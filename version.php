1da33675f8affd091c4c31fc222a634c6a12a442
ralferlebach <ralf.erlebach@gmx.de>
2024-08-14 13:08:59 +0200

    Version Bumb

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_catquiz';
 $plugin->release = '1.0.3';
-$plugin->version = 2024081300;
+$plugin->version = 2024081400;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
