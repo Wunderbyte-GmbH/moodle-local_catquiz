52e7109cc41e00f7c251b6a1971e7e4e864d60f6
ralferlebach <ralf.erlebach@gmx.de>
2024-09-26 21:56:56 +0200

    Big Version Bump

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_catquiz';
-$plugin->release = '1.0.3';
-$plugin->version = 2024082900;
+$plugin->release = '1.1.0';
+$plugin->version = 2024092600;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
