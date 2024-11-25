053941c6d48898a6a687cf7d5adac2a8ea10af11
David Szkiba <david.szkiba@wunderbyte.at>
2024-11-25 13:19:43 +0100

    GH-723 Fix actions errors (PHPUnit and linting)

--- tests/teststrategy/preselect_task/updatepersonability_test.php
+++ tests/teststrategy/preselect_task/updatepersonability_test.php
@@ -109,6 +109,7 @@ final class updatepersonability_test extends TestCase {
                     'contextid' => 1,
                     'catscaleid' => 1,
                     'userid' => $USER->id,
+                    'attemptid' => 123,
                     // Can be null here, because for pilot questions the ability will not be updated.
                     'fake_response_data' => [$USER->id => []],
                 ],
@@ -124,6 +125,7 @@ final class updatepersonability_test extends TestCase {
                         2 => 0.77,
                     ],
                     'contextid' => 1,
+                    'attemptid' => 123,
                     'catscaleid' => 1,
                     'userid' => $USER->id,
                     'questions' => [
