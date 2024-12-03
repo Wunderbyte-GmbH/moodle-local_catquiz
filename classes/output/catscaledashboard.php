19e2dd4c2f84058bdd6baf3b4fa505c45bb67df1
David Szkiba <david.szkiba@wunderbyte.at>
2024-12-03 11:22:15 +0100

    GH-723 Update webservice UI

--- classes/output/catscaledashboard.php
+++ classes/output/catscaledashboard.php
@@ -201,7 +201,7 @@ class catscaledashboard {
 
     private function render_submitresponses_button() {
         // Only render the button for root scales that have no parent scale.
-        if ($this->catscale->parent ?? false) {
+        if ($this->catscale->parent ?: false) {
             return '';
         }
 
