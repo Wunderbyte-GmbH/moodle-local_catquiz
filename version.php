2077e74aa5a9bf4c65047d28477dfb901249d3c7
David Szkiba <david.szkiba@wunderbyte.at>
2024-12-09 12:21:57 +0100

    GH-751 Temporarily remove charts
    Remove charts to find out if they are causing the JS timeout.

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_catquiz';
 $plugin->release = '1.1.2';
-$plugin->version = 2024120500;
+$plugin->version = 2024120900;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
