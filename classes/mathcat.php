97faaaf86409f1e6ade08522a55ceb21ee3549cc
ralferlebach <ralf.erlebach@gmx.de>
2025-01-15 07:06:21 +0100

    Update mathcat.php

--- classes/mathcat.php
+++ classes/mathcat.php
@@ -437,6 +437,7 @@ class mathcat {
                 $mxdelta = $mxgradient->multiply($steplength / $gradientlength);
                 $mxparameternew = $mxparameter->add($mxdelta);
                 $parameternew = (array) $mxparameternew;
+                debugging('mxparameternew: '.print_r($mxparameternew, true), DEBUG_DEVELOPER);
                 debugging('parameternew: '.print_r($parameternew, true), DEBUG_DEVELOPER);
                 debugging('parameternew rebuilt: '.print_r(self::vector_to_array($parameternew, $parameterstructure),
                         true), DEBUG_DEVELOPER);
