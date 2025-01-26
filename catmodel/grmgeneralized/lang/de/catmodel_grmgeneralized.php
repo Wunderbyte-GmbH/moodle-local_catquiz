bdfab257c94bbbe47c14ecef96f4e9fb23bb603a
ralferlebach <ralf.erlebach@gmx.de>
2025-01-26 19:58:37 +0100

    Update

---
+++ catmodel/grmgeneralized/lang/de/catmodel_grmgeneralized.php
@@ -0,0 +1,41 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file contains language strings for the 1plraschbirnbaum model catquiz plugin
+ *
+ * @package catmodel_grmgeneralized
+ * @copyright  2022 Georg Maißer <info@wunderbyte.at>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'generalisiertes Graded Response Model';
+$string['trusted_region_min_a'] = 'Trusted Region Minimum a';
+$string['trusted_region_min_a_desc'] = 'Trusted Region Minimum a';
+$string['trusted_region_max_a'] = 'Trusted Region Maximum a';
+$string['trusted_region_max_a_desc'] = 'Trusted Region Maximum a';
+$string['trusted_region_factor_sd_a'] = 'Trusted Region Factor SD a';
+$string['trusted_region_factor_sd_a_desc'] = 'Use x times of SD as range of trusted regions';
+$string['trusted_region_placement_b'] = "Placement of the discriminatory parameter";
+$string['trusted_region_placement_b_desc'] = "Placement of the discriminatory parameter";
+$string['trusted_region_slope_b'] = "Slope of the discriminatory parameter";
+$string['trusted_region_slope_b_desc'] = "Slope of the discriminatory parameter";
+$string['trusted_region_factor_max_b'] = "Trusted Region Factor placement b";
+$string['trusted_region_factor_max_b_desc'] = "Trusted Region Factor placement b";
+$string['trusted_region_min_b'] = "Trusted region min b";
+$string['trusted_region_min_b_desc'] = "Trusted region min b";
+$string['trusted_region_max_b'] = "Trusted region max b";
+$string['trusted_region_max_b_desc'] = "Trusted region max b";
