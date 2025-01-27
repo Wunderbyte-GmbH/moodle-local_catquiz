de2c25853aa4f08963a2c9f05d84f43889c2a47b
David Szkiba <david.szkiba@wunderbyte.at>
2025-01-27 12:12:38 +0100

    GH-723 Add upgrade script to set scale labels
    The scale synchronization expects scales to have labels. While we
    already set a label when creating a new scale, this adds an upgrade step
    that will update existing scales.

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_catquiz';
 $plugin->release = '1.1.2';
-$plugin->version = 2025012701;
+$plugin->version = 2025012702;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
