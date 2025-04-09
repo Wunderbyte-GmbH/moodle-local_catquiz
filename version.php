1b8868da54b3917b6d82301dc18d6aae788fbbde
Magdalena Holczik <magdalena.holczik@wunderbyte.at>
2025-04-09 09:35:01 +0200

    Version 1.1.3 (2025040900)

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_catquiz';
-$plugin->release = '1.1.2';
-$plugin->version = 2025012702;
+$plugin->release = '1.1.3';
+$plugin->version = 2025040900;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
