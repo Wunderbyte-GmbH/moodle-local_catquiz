f5dc96a34820d5921676b19d15bdcb089489d2c2
ralferlebach <ralf.erlebach@gmx.de>
2025-09-03 10:58:08 +0200

    Bugfix 821, 822 to be reviewed

--- classes/data/dataapi.php
+++ classes/data/dataapi.php
@@ -362,39 +362,90 @@ class dataapi {
      * @param catscale_structure|stdClass $catscale
      * @return bool
      */
+    // public static function update_catscale($catscale): bool {
+    //     global $DB, $USER;
+    //     if (!isset($catscale->id)) {
+    //         throw new moodle_exception('noidset', 'local_catquiz');
+    //     }
+
+    //     $oldrecord = $DB->get_record('local_catquiz_catscales', ['id' => $catscale->id]);
+    //     // If the context of the scale was changed, we have to update the active item params.
+    //     if ($oldrecord->contextid != $catscale->contextid) {
+    //         $repo = new catquiz();
+    //         $repo->create_items_in_new_context($catscale->contextid, $oldrecord->contextid);
+    //     }
+
+    //     $result = $DB->update_record('local_catquiz_catscales', $catscale);
+
+    //     $context = context_system::instance();
+
+    //     $event = catscale_updated::create([
+    //         'objectid' => $catscale->id,
+    //         'context' => $context,
+    //         'userid' => $USER->id, // The user who did cancel.
+    //         'other' => [
+    //             'catscaleid' => $catscale->id,
+    //         ],
+    //     ]);
+    //     $event->trigger();
+
+    //     // Invalidate cache. TODO: Instead of invalidating cache, delete and add the item from the cache.
+    //     $cache = cache::make('local_catquiz', 'catscales');
+    //     $cache->delete('allcatscales');
+    //     $cache->delete($catscale->id);
+    //     return $result;
+    // }
+
+    /**
+     *  Version mit Fehlerbehebung
+     */
     public static function update_catscale($catscale): bool {
         global $DB, $USER;
         if (!isset($catscale->id)) {
             throw new moodle_exception('noidset', 'local_catquiz');
         }
-
+    
         $oldrecord = $DB->get_record('local_catquiz_catscales', ['id' => $catscale->id]);
-        // If the context of the scale was changed, we have to update the active item params.
-        if ($oldrecord->contextid != $catscale->contextid) {
-            $repo = new catquiz();
-            $repo->create_items_in_new_context($catscale->contextid, $oldrecord->contextid);
+        // Falls Datensatz fehlt, lieber fail-fast mit klarer Meldung:
+        if (!$oldrecord) {
+            debugging("local_catquiz: update_catscale: scale {$catscale->id} not found", DEBUG_DEVELOPER);
+            return false;
         }
-
+    
+        // Nur wenn alter Kontext vorhanden UND anders ist.
+        if (!empty($oldrecord->contextid) && $oldrecord->contextid != $catscale->contextid) {
+            $repo = new catquiz();
+            try {
+                $repo->create_items_in_new_context($catscale->contextid, $oldrecord->contextid);
+            }
+            catch (\Throwable $e) {
+                debugging(
+                    "local_catquiz: create_items_in_new_context failed for scale {$catscale->id} ".
+                    "(new={$catscale->contextid}, old={$oldrecord->contextid}): ".$e->getMessage(),
+                    DEBUG_DEVELOPER
+                );
+                // „Best effort“: nicht hart abbrechen, damit Cron durchläuft.
+            }
+        }
+    
         $result = $DB->update_record('local_catquiz_catscales', $catscale);
-
+    
         $context = context_system::instance();
-
+        
         $event = catscale_updated::create([
             'objectid' => $catscale->id,
-            'context' => $context,
-            'userid' => $USER->id, // The user who did cancel.
-            'other' => [
-                'catscaleid' => $catscale->id,
-            ],
+            'context'  => $context,
+            'userid'   => $USER->id,
+            'other'    => ['catscaleid' => $catscale->id],
         ]);
         $event->trigger();
-
-        // Invalidate cache. TODO: Instead of invalidating cache, delete and add the item from the cache.
+    
         $cache = cache::make('local_catquiz', 'catscales');
         $cache->delete('allcatscales');
         $cache->delete($catscale->id);
         return $result;
     }
+
 
     /**
      * Check if name of catscale already exsists - must be unique
