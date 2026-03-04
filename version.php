2edb900470340dc30c642b6a999f9577dddafc80
jacob.viertel@wunderbyte.at <jacob.viertel@wunderbyte.at>
2026-03-04 07:45:08 +0100

    Bugfix: Correction of required moodle version

--- version.php
+++ version.php
@@ -33,5 +33,4 @@ $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
     'local_wunderbyte_table' => 2024040200,
     'mod_adaptivequiz' => 2024031502,
-    'adaptivequizcatmodel_catquiz' => 2024062800,
 ];
